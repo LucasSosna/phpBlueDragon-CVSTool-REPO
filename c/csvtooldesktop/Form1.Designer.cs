aedda150c48c85efde9690b8fe16f14b1b4a1e6b
Lucas Sosna <lukasz@lukasz.sos.pl>
2020-05-15 18:44:48 +0200

    Added program C#

---
+++ c/csvtooldesktop/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace csvtooldesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.textBoxSeparator = new System.Windows.Forms.TextBox();
+            this.textBoxOutFile = new System.Windows.Forms.TextBox();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.buttonMakeTable = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wWWToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File:\r\n- Each line should contain a separate line\r\n- Each row should contain the " +
+    "same number of columns\r\n- The file will be converted into a table using the <tab" +
+    "le> tag.";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Enabled = false;
+            this.textBoxFile.Location = new System.Drawing.Point(15, 102);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(232, 20);
+            this.textBoxFile.TabIndex = 1;
+            // 
+            // textBoxSeparator
+            // 
+            this.textBoxSeparator.Location = new System.Drawing.Point(253, 102);
+            this.textBoxSeparator.Name = "textBoxSeparator";
+            this.textBoxSeparator.Size = new System.Drawing.Size(50, 20);
+            this.textBoxSeparator.TabIndex = 2;
+            this.textBoxSeparator.Text = ";";
+            // 
+            // textBoxOutFile
+            // 
+            this.textBoxOutFile.Location = new System.Drawing.Point(15, 157);
+            this.textBoxOutFile.Multiline = true;
+            this.textBoxOutFile.Name = "textBoxOutFile";
+            this.textBoxOutFile.Size = new System.Drawing.Size(369, 216);
+            this.textBoxOutFile.TabIndex = 3;
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(309, 102);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectFile.TabIndex = 4;
+            this.buttonSelectFile.Text = "Browse file";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // buttonMakeTable
+            // 
+            this.buttonMakeTable.Location = new System.Drawing.Point(15, 128);
+            this.buttonMakeTable.Name = "buttonMakeTable";
+            this.buttonMakeTable.Size = new System.Drawing.Size(369, 23);
+            this.buttonMakeTable.TabIndex = 5;
+            this.buttonMakeTable.Text = "Make table";
+            this.buttonMakeTable.UseVisualStyleBackColor = true;
+            this.buttonMakeTable.Click += new System.EventHandler(this.buttonMakeTable_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(398, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wWWToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // wWWToolStripMenuItem1
+            // 
+            this.wWWToolStripMenuItem1.Name = "wWWToolStripMenuItem1";
+            this.wWWToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.wWWToolStripMenuItem1.Text = "WWW";
+            this.wWWToolStripMenuItem1.Click += new System.EventHandler(this.wWWToolStripMenuItem1_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "WWW - Author";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 412);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonMakeTable);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.textBoxOutFile);
+            this.Controls.Add(this.textBoxSeparator);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CSVToolDesktop";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.TextBox textBoxSeparator;
+        private System.Windows.Forms.TextBox textBoxOutFile;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Button buttonMakeTable;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wWWToolStripMenuItem1;
+    }
+}
+
---
+++ c/csvtooldesktop/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csvtooldesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private string[] linesOfFile;
+
+        private void buttonSelectFile_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv|TXT files (*.txt)|*.txt";
+                //openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+                    var fileStream = openFileDialog.OpenFile();
+
+                    textBoxFile.Text = filePath;
+
+                    /*using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }*/
+
+                    linesOfFile = File.ReadAllLines(filePath);
+                }
+            }
+        }
+
+        private void buttonMakeTable_Click(object sender, EventArgs e)
+        {
+            string OutPut = "";
+
+            if (textBoxSeparator.Text == "")
+            {
+                MessageBox.Show("No sign of record division.", "CVSToolDesign", MessageBoxButtons.OK);
+            }
+            else
+            {
+                if (textBoxFile.Text == "")
+                {
+                    MessageBox.Show("No file has been added.", "CVSToolDesign", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    OutPut = "<table>\r\n";
+
+                    foreach (string oneLine in linesOfFile)
+                    {
+                        string[] ElementsOfLine = oneLine.Split(textBoxSeparator.Text[0]);
+
+                        OutPut += "<tr>\r\n";
+
+                        foreach (string oneCharLines in ElementsOfLine)
+                        {
+                            OutPut += "<td>" + oneCharLines + "</td>\r\n";
+                        }
+
+                        OutPut += "</tr>\r\n";
+                    }
+
+                    OutPut += "</table>";
+
+                    textBoxOutFile.Text = OutPut;
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void wWWToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://edytor.warszawa.pl/");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://phpbluedragon.eu/");
+        }
+    }
+}
